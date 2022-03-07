05bb937d796f4db3d5b48e29b6774368b0286d23
matheus-nunes <matheus.nmreg@gmail.com>
2022-03-06 23:00:02 -0300

    main

--- src/routes.js
+++ src/routes.js
@@ -5,21 +5,13 @@ const routes = express.Router()
 //request é perguntar
 // joguinho de pergunta e resposta
 
-const basePath = __dirname + '/views' // isso aqui serve para encurtar o dir name
+//const basePath = __dirname + '/views/' // isso aqui serve para encurtar o dir name
 
 //MONTANDO As ROTAS    <<-- --
-routes.get('/', (request, response) =>
-  response.sendFile(basePath + '/index.html')
-)
+routes.get('/', (request, response) => response.render('index'))
 // quando for o metodo get no barra. quero que no segundo argumento execute uma função de novo
-routes.get('/job', (request, response) =>
-  response.sendFile(basePath + '/job.html')
-)
-routes.get('/job-edit.html', (request, response) =>
-  response.sendFile(basePath + '/job-edit.html')
-)
-routes.get('/profile', (request, response) =>
-  response.sendFile(basePath + '/profile.html')
-)
+routes.get('/job', (request, response) => response.render('job'))
+routes.get('/job-edit', (request, response) => response.render('job-edit'))
+routes.get('/profile', (request, response) => response.render('profile'))
 
 module.exports = routes
